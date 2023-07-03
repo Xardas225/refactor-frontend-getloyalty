64b0596cdd2cc75f7612b14f8ffdcf4d97a49644
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-07-03 19:34:05 +0300

    install bootstrap 4, font-awesome core

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
