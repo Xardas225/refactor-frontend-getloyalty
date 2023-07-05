b2394971df7e837aab6a898df208451963ad9d63
Xardas225 <alexanderderevkov@yandex.ru>
2023-07-05 13:01:39 +0300

    create bc component, add in profile

---
+++ src/components/ui/BreadCrumbs.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface BreadcrumbItem {
+  title: string;
+  active: boolean;
+
+}
+
+const props = defineProps<{
+  items: Array<BreadcrumbItem>
+}>();
+
+</script>
+
+<template>
+  <div class="row px-2 justify-content-between">
+    <ol class="breadcrumb">
+      <li
+        v-for="item in items"
+        :key="item.title"
+        class="breadcrumb-item"
+        :class="{ 'active': item.active }"
+      >
+        {{ item.title }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<style scoped>
+.breadcrumb {
+  background-color: transparent;
+}
+</style>
---
+++ src/views/settings/ProfileView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import GeneralSection from '../../components/profile/GeneralSection.vue';
+import BreadCrumbs from "@/components/ui/BreadCrumbs.vue";
+
+const breadcrumbData = [
+  {
+    title: 'Настройки',
+    active: false,
+  },
+  {
+    title: 'Профиль',
+    active: true,
+  }
+]
+</script>
+
+
+<template>
+  <BreadCrumbs :items="breadcrumbData"/>
+  <div class="tab-content card">
+    <GeneralSection />
+  </div>
+</template>
