0f15f79ce3091ae9f0f92996071738a7eaf87184
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-07-16 11:46:23 +0300

    add reviewsSection

---
+++ src/components/tape/ReviewSection.vue
@@ -0,0 +1,130 @@
+<script setup>
+import { defineProps } from "vue";
+
+interface ReviewData {
+  id: number;
+  source_hash_key: string;
+  date: number;
+  rating: number;
+  tonal: string;
+  is_other_comments: boolean;
+  user_name: string;
+  review_link: string | null;
+  text: string;
+  first_comment: string | null;
+  user_link: string | null;
+  user_avatar_link: string;
+  created_at: string;
+  updated_at: string;
+  external_id: string;
+  params: string | null;
+  classes: string | null;
+  transit_replier_meta: string | null;
+  deleted_at: string | null;
+  is_classified: number;
+}
+
+const review = defineProps<ReviewData>();
+</script>
+
+<template>
+  <div id="review_card_pattern" class="card d-none">
+    <div class="review-body d-flex flex-row align-items-top p-4">
+      <div class="review-avatar">
+        <img
+          :src="review.user_avatar_link || '/img/user/placeholder.png'"
+          alt=""
+          class="d-block ui-w-50 ui-h-50 rounded-circle"
+        />
+      </div>
+      <div class="review-content w-100 pl-2">
+        <div class="d-flex justify-content-between">
+          <div>
+            <span class="review-fio font-weight-semibold">{{ review.user_name }}</span>
+            <span>·</span>
+            <span class="text-muted">
+              <span class="review-time"></span>
+              <span class="review-date"></span>
+            </span>
+            <i
+              class="far fas-manage fa-question-circle review-detected"
+              data-toggle="popover"
+              data-placement="bottom"
+            ></i>
+          </div>
+          <div class="review-rating">
+            <div class="ui-stars text-nowrap">
+              <div class="ui-star">
+                <i class="ion ion-md-star"></i>
+                <i class="ion ion-md-star"></i>
+              </div>
+              <div class="ui-star">
+                <i class="ion ion-md-star"></i>
+                <i class="ion ion-md-star"></i>
+              </div>
+              <div class="ui-star">
+                <i class="ion ion-md-star"></i>
+                <i class="ion ion-md-star"></i>
+              </div>
+              <div class="ui-star">
+                <i class="ion ion-md-star"></i>
+                <i class="ion ion-md-star"></i>
+              </div>
+              <div class="ui-star">
+                <i class="ion ion-md-star"></i>
+                <i class="ion ion-md-star"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="review-text py-2"></div>
+        <div class="review-source d-flex flex-wrap text-muted">
+          <span class="review-platform mr-2"></span>
+          <span>·</span>
+          <span class="review-filial ml-2 mr-2"></span>
+          <span class="external d-none">
+            <span>·</span>
+            <a class="ml-2 text-muted" href="" target="_blank"
+              >Перейти к отзыву</a
+            >
+          </span>
+        </div>
+        <div class="review-classes pt-2"></div>
+        <div class="review-answered-wrap pt-2">
+          <div class="review-comments d-none"></div>
+          <div class="review-comments-controls d-none">
+            <button
+              type="button"
+              class="btn btn-outline-primary review-comment-toggle"
+              data-toggle="show"
+            >
+              <i class="fas fa-chevron-down"></i>Показать обсуждение
+            </button>
+          </div>
+        </div>
+        <div class="review-reply d-none">
+          <div class="form-group mb-1">
+            <label class="form-label">Текст ответа</label>
+            <textarea class="form-control" rows="3"></textarea>
+          </div>
+          <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-primary mr-2 reply-send">
+              <i class="d-none"></i>Отправить ответ
+            </button>
+            <button type="button" class="btn btn-default mr-2 reply-close">
+              Отмена
+            </button>
+          </div>
+        </div>
+        <div class="review-controls d-flex justify-content-end">
+          <button type="button" class="btn btn-primary mr-2 integration d-none">
+            <i class="fas fa-comment-dots"></i>Ответить
+          </button>
+          <button type="button" class="btn btn-primary d-none" disabled="">
+            <i class="fas fa-exclamation-triangle"></i>Пожаловаться
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/store/reviews-store.ts
+++ src/store/reviews-store.ts
@@ -8,10 +8,33 @@ interface MetaData {
   has_answer: number;
   rating: number[];
 }
+interface ReviewData {
+  id: number;
+  source_hash_key: string;
+  date: number;
+  rating: number;
+  tonal: string;
+  is_other_comments: boolean;
+  user_name: string;
+  review_link: string | null;
+  text: string;
+  first_comment: string | null;
+  user_link: string | null;
+  user_avatar_link: string;
+  created_at: string;
+  updated_at: string;
+  external_id: string;
+  params: string | null;
+  classes: string | null;
+  transit_replier_meta: string | null;
+  deleted_at: string | null;
+  is_classified: number;
+}
 
 // Определение интерфейса для состояния
 interface ReviewsState {
   meta: MetaData;
+  reviews: Array<ReviewData>
 }
 
 export const useReviewsStore = defineStore('reviews-store', {
@@ -24,6 +47,7 @@ export const useReviewsStore = defineStore('reviews-store', {
       has_answer: 0,
       rating: [0, 0, 0, 0, 0, 0],
     },
+    reviews: []
   }),
   getters: {
 
@@ -39,9 +63,62 @@ export const useReviewsStore = defineStore('reviews-store', {
           has_answer: 185,
           rating: [18, 27, 5, 9, 26, 202],
         };
-        this.meta = {...data, rating: data.rating.reverse()}
+        this.meta = { ...data, rating: data.rating.reverse() }
       } catch (error: any) {
         console.error(error.message)
+      }
+    },
+    async getReviewData() {
+      try {
+        // fetch data
+        this.reviews = [
+          {
+            "deleted_at": null,
+            "id": 36759394,
+            "source_hash_key": "b034d8fb8601be4402d55ee98b272bc7",
+            "date": 1688342400,
+            "rating": 5,
+            "tonal": "POSITIVE",
+            "is_other_comments": false,
+            "user_name": "Таир",
+            "review_link": null,
+            "text": "Все отлично!",
+            "first_comment": null,
+            "user_link": null,
+            "user_avatar_link": "https://50.img.avito.st/image/1/1.o7w1kLa2GVUDN41Tf7aRgdwzD1-LswnXhzMN.uIhsHW-5wwEsIKEhI5JIQ7IBdCzJCLmwp_L3dX954Pw",
+            "created_at": "2023-07-03T14:38:59.000000Z",
+            "updated_at": "2023-07-03T14:40:55.000000Z",
+            "external_id": "3c94ea1aab86d4b5557493accb11ddae",
+            "params": null,
+            "is_classified": 1,
+            "classes": null,
+            "transit_replier_meta": null
+          },
+          {
+            "deleted_at": null,
+            "id": 36764422,
+            "source_hash_key": "da0cbab1571b855b7e5efe949e9c2d63",
+            "date": 1688316598,
+            "rating": 5,
+            "tonal": "POSITIVE",
+            "is_other_comments": false,
+            "user_name": "Марина Баркова",
+            "review_link": null,
+            "text": "Ходили с мамой на Мадмуазель Нитушь, просто сказка, позитивнейшие эмоции, шикарные костюмы, Мария Аронова просто бесподобна! Сам театр красивый, есть залы истории театра, очень интересно, красиво и познавательно.  Посетили исторический буфет, все замечательно каждый прекрасно может провести время.",
+            "first_comment": null,
+            "user_link": null,
+            "user_avatar_link": "https://avatars.mds.yandex.net/get-yapic/63032/fmfh3OUZZ8pea1dRnVWkMVEMEBs-1/islands-68",
+            "created_at": "2023-07-03T16:32:02.000000Z",
+            "updated_at": "2023-07-03T16:33:26.000000Z",
+            "external_id": "7YAILKlwlD6QlCvu8PjYCvpr5SzS54jv",
+            "params": null,
+            "is_classified": 1,
+            "classes": "{\"name\":true,\"tone\":true,\"term\":true}",
+            "transit_replier_meta": null
+          }
+        ]
+      } catch (error) {
+        console.error(error)
       }
     }
   },
